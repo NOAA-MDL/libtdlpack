b7eb5407d40f91fd4ec7874eb9514198b936fc82
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2025-03-02 19:30:16 -0500

    Update test_open_tdlpack_file.c

--- tests/test_open_tdlpack_file.c
+++ tests/test_open_tdlpack_file.c
@@ -6,6 +6,7 @@ int main()
 {
    int32_t kstdout=6;
 
+   printf("Test creating a small-template random access file...");
    {
       char name[] = "test_ra_small.tdlp";
       char mode[] = "w";
@@ -15,10 +16,11 @@ int main()
       int32_t ier=0;
       char ra_template[] = "small";
       open_tdlpack_file(&kstdout, name, mode, &lun, &byteorder, &filetype, &ier, ra_template);
-      printf("on return, lun = %d\n", lun);
-      return ier;
+      if (ier != 0)
+         return ier;
    }
-
+   printf(" SUCCESS!\n");
+   printf("Test creating a large-template random access file...");
    {
       char name[] = "test_ra_large.tdlp";
       char mode[] = "w";
@@ -28,8 +30,23 @@ int main()
       int32_t ier=0;
       char ra_template[] = "large";
       open_tdlpack_file(&kstdout, name, mode, &lun, &byteorder, &filetype, &ier, ra_template);
-      printf("on return, lun = %d\n", lun);
-      return ier;
+      if (ier != 0)
+         return ier;
    }
+   printf(" SUCCESS!\n");
+   printf("Test creating a sequential file...");
+   {
+      char name[] = "test_sq.tdlp";
+      char mode[] = "w";
+      int32_t byteorder=1; // 1 = Big-Endian
+      int32_t filetype=2; // 2 = SQ
+      int32_t lun=0;
+      int32_t ier=0;
+      open_tdlpack_file(&kstdout, name, mode, &lun, &byteorder, &filetype, &ier, NULL);
+      if (ier != 0)
+         return ier;
+   }
+   printf(" SUCCESS!\n");
+
    return 0;
 }
