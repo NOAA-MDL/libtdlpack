e1304c160fa23db631e8f4994ef92346d1f84d51
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2025-05-07 22:01:33 -0400

    Moved RA template MAXENT and NBYTES values to tdlpack_mod.f90.

--- src/open_tdlpack_file.f90
+++ src/open_tdlpack_file.f90
@@ -123,11 +123,11 @@ elseif(mode1.eq."w".or.mode1.eq."x")then
       ! Random-Access
       if(present(ra_template))then
          if(f_ra_template.eq."small")then
-            maxent=300
-            nbytes=2000
+            maxent=TDLP_RA_MAXENT_SMALL
+            nbytes=TDLP_RA_NBYTES_SMALL
          elseif(f_ra_template.eq."large")then
-            maxent=840
-            nbytes=20000
+            maxent=TDLP_RA_MAXENT_LARGE
+            nbytes=TDLP_RA_NBYTES_LARGE
          endif
          call createra(kstdout,f_file,TDLP_L3264B,lun,maxent,nbytes,ier)
       endif
